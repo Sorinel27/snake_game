4a79824caf78e4d2f6fe09c1b3a8aead42953252
Sorinel27 <sorin78910@gmail.com>
2022-10-19 17:06:34 +0300

    create food and scoreboard classes and making the game more playable

--- main.py
+++ main.py
@@ -1,44 +1,14 @@
 from turtle import Screen, Turtle
 from snake import Snake
-import turtle as t
+from food import Food
+from scoreboard import Score
 import time
 
 marcel = Turtle()
 screen = Screen()
+food = Food()
 parts = []
-
-
-def up():
-    speed = 10
-    for part in parts:
-        part.fd(speed)
-        part.left(90)
-        speed += 10
-
-
-def left():
-    for i in reversed(range(len(parts))):
-        x = parts[i - 1].xcor()
-        y = parts[i - 1].ycor()
-        parts[i].goto(x, y)
-    parts[0].left(90)
-    parts[0].fd(20)
-
-
-def right():
-    speed = 10
-    for part in parts:
-        part.fd(speed)
-        part.right(90)
-        speed += 10
-
-
-def down():
-    speed = 10
-    for part in parts:
-        part.fd(speed)
-        part.left(90)
-        speed += 10
+score = Score()
 
 
 def main():
@@ -48,10 +18,26 @@ def main():
     screen.title("Snake game")
     screen.tracer(0)
     snake = Snake()
+    screen.listen()
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "d")
     is_not_dead = True
     while is_not_dead:
         screen.update()
         time.sleep(0.1)
+        if snake.head.distance(food) < 15:
+            food.random_location()
+            score.update()
+            snake.new_part()
+        for i in range(len(snake.parts) - 1, 1, -1):
+            if snake.head.distance(snake.parts[i]) < 10:
+                is_not_dead = False
+                score.game_over()
+        if snake.head.xcor() > 330 or snake.head.ycor() > 330 or snake.head.xcor() < -330 or snake.head.ycor() < -330:
+            is_not_dead = False
+            score.game_over()
         snake.move()
     screen.exitonclick()
 
--- snake.py
+++ snake.py
@@ -1,10 +1,14 @@
 from turtle import Turtle
 
+positions = [(0, 0), (-20, 0), (-40, 0)]
+speed = 20
+angle = 90
 
-class Snake:
+
+class Snake(Turtle):
     def __init__(self):
+        super().__init__()
         parts = []
-        positions = [(0, 0), (-20, 0), (-40, 0)]
         for pos in positions:
             part = Turtle("square")
             part.color("white")
@@ -12,11 +16,36 @@ class Snake:
             part.goto(pos)
             parts.append(part)
         self.parts = parts
+        self.head = self.parts[0]
 
     def move(self):
         for part in range(len(self.parts) - 1, 0, -1):
             x = self.parts[part - 1].xcor()
             y = self.parts[part - 1].ycor()
             self.parts[part].goto(x, y)
-        self.parts[0].forward(20)
-        self.parts[0].left(90)
+        self.head.forward(speed)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def new_part(self):
+        part = Turtle("square")
+        part.color("white")
+        part.pu()
+        x = self.parts[len(self.parts) - 1].xcor()
+        y = self.parts[len(self.parts) - 1].ycor()
+        part.goto(x, y)
+        self.parts.append(part)
